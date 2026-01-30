b94f16625c495bee019732caa52cf1eb71135880
mosheng-eng <131440881+mosheng-eng@users.noreply.github.com>
2026-01-30 15:09:33 +0800

    Update TimePowerLoan.sol

--- src/protocols/borrower/time-power/TimePowerLoan.sol
+++ src/protocols/borrower/time-power/TimePowerLoan.sol
@@ -8,8 +8,6 @@ import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/Pau
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
@@ -23,7 +21,6 @@ import {Errors} from "src/common/Errors.sol";
 
 contract TimePowerLoan is Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable, PausableUpgradeable {
     using Math for uint256;
-    using SafeCast for uint256;
     using SafeERC20 for IERC20;
     using TimePowerLoanLibs for TimePowerLoanDefs.DebtInfo[];
     using TimePowerLoanLibs for TimePowerLoanDefs.LoanInfo[];
