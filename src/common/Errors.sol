918ab5158f0588288549ff411eefdca97a11b1cb
mosheng-eng <131440881+mosheng-eng@users.noreply.github.com>
2026-05-17 09:43:54 +0800

    add a new error
    add a new error

--- src/common/Errors.sol
+++ src/common/Errors.sol
@@ -9,18 +9,28 @@ library Errors {
     /// @dev Reverted when an address is zero.
     /// @param name The name of the address variable that is zero.
     error ZeroAddress(string name);
+
     /// @dev Reverted when a value is invalid.
     /// @param name The name of the value variable that is invalid.
     error InvalidValue(string name);
+
     /// @dev Reverted when contract is not fully initialized.
     /// @param name The field name of the contract that is uninitialized.
     error Uninitialized(string name);
+
     /// @dev Reverted when balance is not enough for a transfer.
     /// @param balance The current balance.
     /// @param transfer The amount to be transferred.
     error InsufficientBalance(uint256 balance, uint256 transfer);
+
     /// @dev Reverted when allowance is not enough for a transfer.
     /// @param allowance The current allowance.
     /// @param required The required allowance for the transfer.
     error InsufficientAllowance(uint256 allowance, uint256 required);
+
+    /// @dev Reverted when owner does not approve the spender for a token.
+    /// @param owner The owner of the token.
+    /// @param spender The address trying to spend the token.
+    /// @param tokenId The ID of the token being spent.
+    error InsufficientAllowance(address owner, address spender, uint256 tokenId);
 }
